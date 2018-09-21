553ce8e3bac10f0d60e70dd434e56cb524c7dade
FatolaAbdullahi <42966079+FatolaAbdullahi@users.noreply.github.com>
2018-09-21 23:37:57 +0200

    Week3Content

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+function switchChannel(channelName) {
+    document.getElementById('#chat-app-bar').innerHTML='Tuning into channel' + document.getElementById.param(Yummy) + 'by' + 'upgrading.never.helps';
+}
+console.log(switchChannel);
